636c187cd4b364392ee5bde85403a3383749df42
Yaron Cohen <yaroncnk@gmail.com>
2016-10-10 18:00:55 +0000

    hello world recursion

--- hello-world.js
+++ hello-world.js
@@ -5,4 +5,17 @@ setTimeout(function() {
   console.log('Hello world again!');
   }, 10000);
   
+//q2 hello world to infinity
+
+function infi() {
+  setTimeout(function(){
+    console.log('Hello world');
+    return infi();
+  }, 2000);
+  return 'Hello world'
+}
+
+
+
+  
   
